31bc05e1b041d9c80ce2cde78bf661e9782c9452
hacker.do <hacker.do@163.com>
2017-12-19 16:00:27 +0800

    加入海思图像处理库及对应头文件, 编译通过.

--- src/ui/main.cpp
+++ src/ui/main.cpp
@@ -1,8 +1,13 @@
 #include "ui_mainwindow.h"
 
+#include "mpi/hi_comm_sys.h"
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    // 测试加入海思图像处理库编译是否通过
+    MPP_SYS_CONF_S stSysConf = { 0 };
 
     QMainWindow w;
     Ui::MainWindow ui;
